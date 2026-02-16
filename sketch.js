697696ab067510b6afbfe0cea82bf314fabe12f1
dpekar456 <dpekar@ucsc.edu>
2026-02-15 17:54:07 -0800

    Add files via upload
    Here's the beginning of my second project

--- sketch.js
+++ sketch.js
@@ -1,81 +1,16 @@
-let spin = 0;
-let userSpin = 0
-let lastMouseX = 0
-let dragging = false
-
 function setup() {
-  createCanvas(400, 400, WEBGL);
-  colorMode(HSB,360,100,100);
-angleMode(RADIANS);
+  colorMode(HSL)
+  createCanvas(400, 400);
 }
 
-function mousePressed(){
-  dragging = true
-  lastMouseX = mouseX
-}
-function mouseReleased(){
-  dragging = false
-  
-}
 function draw() {
-  background(0)
-  
-   print(hour(),minute(),second())
-  let s = map(second(),0,60,TWO_PI,0);
-  let m = map(minute() % 60 + second()/60 , 0, 60, TWO_PI, 0);
-  let h = map(hour() % 12 + minute() / 60 ,0,12,TWO_PI,0);
-
-  let spinSpeed = map(second(), 0, 60, 0.05, 0.002);
-  spin += spinSpeed;
-  
-  if (dragging) {
-    let dx = mouseX - lastMouseX
-    userSpin += dx * 0.06;
-    lastMouseX = mouseX;
-  }
-  
-push();
-  rotateZ(spin + userSpin);
-  let hue = map(second(),0,60,360,0)
-  fill(hue,100,100)
-noStroke();
-pinwheel(90,12)
-pop();
-  
-  stroke(0,0,100);
-  strokeCap(ROUND)
-  
-  push();
-  rotateZ(s);
-  strokeWeight(2)
-  line(0,0,0,-100)
-  pop();
-  
-  push();
-  rotateZ(m)
-  strokeWeight(4)
-  line(0,0,0,-80)
-  pop();
-  
-  push();
-  rotateZ(h)
-  strokeWeight(6)
-  line(0,0,0,-60)
-  pop();
-  
+  background(220);
   noStroke()
-  fill(0,0,100)
-circle(0,0,20)
-
   
-function pinwheel(size,blades) {
-  let step = TWO_PI/blades;
-  for (let i=0; i < blades;i++){
-    push()
-    rotateZ(i*step);
+  let w = 400/step
+  for (let i = 0; i < step; i++)
+let O = color(19,78.2,38.1)
+let B = color(212,46.9,68)
   
-  quad (0,0,size,0,size,size,0,size)
-    pop();
-  }
-}}
-
+  
+}
